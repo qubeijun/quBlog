229045a238fa45c916d75d60128fd9968bf36a2e
qubeijun <825954789@qq.com>
2020-05-28 19:02:51 +0800

    init

--- docs/.vuepress/components/BorderRadiusPreviewer.vue
+++ docs/.vuepress/components/BorderRadiusPreviewer.vue
@@ -4,23 +4,35 @@
       <el-main class="bordermain">
         <el-row type="flex" justify="center">
           <el-col :span="8">
-            <el-slider class="top" v-model="value1"></el-slider>
+            <el-slider v-model="value1" class="top" @input="handleTop(value1)" />
           </el-col>
         </el-row>
         <el-row type="flex" justify="center">
           <el-col :span="8">
-            <el-slider class="left" v-model="value2" vertical></el-slider>
+            <el-slider v-model="value2" class="left" vertical @input="handleLeft(value2)" />
           </el-col>
           <el-col :span="8">
-            <div class="middle" ></div>
+            <div
+              ref="middle"
+              :style="{ 'border-top-left-radius': borderTopLeftRadius,
+                        'border-top-right-radius': borderTopRightRadius,
+                        'border-bottom-right-radius': borderBottomRightRadius,
+                        'border-bottom-left-radius': borderBottomLeftRadius}"
+              class="middle"
+            />
+            <!-- <div
+              ref="middle"
+              :style="{ 'border-radius': borderRadius}"
+              class="middle"
+            /> -->
           </el-col>
           <el-col :span="8">
-            <el-slider class="right" v-model="value3" vertical></el-slider>
+            <el-slider v-model="value3" class="right" vertical @input="handleRight(value3)" />
           </el-col>
         </el-row>
         <el-row type="flex" justify="center">
           <el-col :span="8">
-            <el-slider class="bottom" v-model="value4"></el-slider>
+            <el-slider v-model="value4" class="bottom" @input="handleBottom(value4)" />
           </el-col>
         </el-row>
       </el-main>
@@ -30,49 +42,91 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      value1: 50,
-      value2: 50,
-      value3: 50,
-      value4: 50,
+      borderTopLeftRadius: '30% 30%',
+      borderTopRightRadius: '70% 30%',
+      borderBottomRightRadius: '70% 70%',
+      borderBottomLeftRadius: '30% 70%',
+      value1: 30,
+      value2: 70,
+      value3: 70,
+      value4: 30
     }
   },
   mounted: function() {},
-  methods:{}
+  methods: {
+    handleTop(value) {
+      var _borderTopLeftRadius = this.$refs.middle.style.borderTopLeftRadius.split(' ')
+      var _borderTopRightRadius = this.$refs.middle.style.borderTopRightRadius.split(' ')
+      _borderTopLeftRadius[0] = value.toString() + '%'
+      _borderTopRightRadius[0] = (100 - value).toString() + '%'
+      this.borderTopLeftRadius = _borderTopLeftRadius.join(' ')
+      this.borderTopRightRadius = _borderTopRightRadius.join(' ')
+    },
+    handleLeft(value) {
+      var _borderTopLeftRadius = this.$refs.middle.style.borderTopLeftRadius.split(' ')
+      var _borderBottomLeftRadius = this.$refs.middle.style.borderBottomLeftRadius.split(' ')
+      _borderTopLeftRadius[1] = (100 - value).toString() + '%'
+      _borderBottomLeftRadius[1] = value.toString() + '%'
+      this.borderTopLeftRadius = _borderTopLeftRadius.join(' ')
+      this.borderBottomLeftRadius = _borderBottomLeftRadius.join(' ')
+    },
+    handleRight(value) {
+      var _borderTopRightRadius = this.$refs.middle.style.borderTopRightRadius.split(' ')
+      var _borderBottomRightRadius = this.$refs.middle.style.borderBottomRightRadius.split(' ')
+      _borderTopRightRadius[1] = (100 - value).toString() + '%'
+      _borderBottomRightRadius[1] = value.toString() + '%'
+      this.borderTopRightRadius = _borderTopRightRadius.join(' ')
+      this.borderBottomRightRadius = _borderBottomRightRadius.join(' ')
+    },
+    handleBottom(value) {
+      var _borderBottomRightRadius = this.$refs.middle.style.borderBottomRightRadius.split(' ')
+      var _borderBottomLeftRadius = this.$refs.middle.style.borderBottomLeftRadius.split(' ')
+      _borderBottomLeftRadius[0] = value.toString() + '%'
+      _borderBottomRightRadius[0] = (100 - value).toString() + '%'
+      this.borderBottomRightRadius = _borderBottomRightRadius.join(' ')
+      this.borderBottomLeftRadius = _borderBottomLeftRadius.join(' ')
+    }
+  }
 }
 </script>
 
 <style>
-.bordermain{
+.bordermain {
   padding: 20px 0px 20px 0px;
   width: 600px;
 }
-.top{
+.top {
   width: 200px;
 }
-.left{
+.left {
   width: 200px;
   height: 200px;
 }
-.left.is-vertical .el-slider__runway{
+.left.is-vertical .el-slider__runway {
   margin: 0 0 0 200px;
 }
-.middle{
+.middle {
+  width: 200px;
+  height: 200px;
+  border-top-left-radius: 30% 30%;
+  border-top-right-radius: 70% 30%;
+  border-bottom-right-radius: 70% 70%;
+  border-bottom-left-radius: 30% 70%;
+  background: #55b9f3;
+}
+.right {
   width: 200px;
   height: 200px;
 }
-.right{
-  width: 200px;
-  height: 200px;
-}
-.right.is-vertical .el-slider__runway{
+.right.is-vertical .el-slider__runway {
   margin: 0 0 0 0px;
 }
-.bottom{
+.bottom {
   width: 200px;
 }
-.el-slider__bar{
-  background-color: #E4E7ED;
+.el-slider__bar {
+  background-color: #e4e7ed;
 }
 </style>
