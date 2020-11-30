58f46745d830b4df0037d8e9fd6eed25a712e55a
qubeijun <825954789@qq.com>
2020-11-30 15:24:14 +0800

    init

--- docs/.vuepress/components/VueTest.vue
+++ docs/.vuepress/components/VueTest.vue
@@ -1,9 +1,45 @@
 <template>
   <div>
     <h1>模版语法</h1>
-    <p>Message: {{ msg }}</p>
-    <p v-once>这个将不会改变：{{ msg }}</p>
+    <h2>插值</h2>
+    <h3>文本</h3>
+    <p>这个将会改变: {{ message }}</p>
+    <p v-once>
+      这个将不会改变：{{ message }}
+      <el-tag effect="dark">v-once：执行一次性地插值</el-tag>
+    </p>
     <el-button @click="handleChange">change</el-button>
+    <h3>原始HTML</h3>
+    <p>Using mustaches: {{ rawHtml }}</p>
+    <p>
+      Using v-html directive:
+      <span v-html="rawHtml"></span>
+      <el-tag effect="dark">v-html：输出真正的html</el-tag>
+    </p>
+    <h3>Attribute</h3>
+    <el-button v-bind:disabled="Disabled1">Disabled1==null</el-button>
+    <el-button v-bind:disabled="Disabled2">Disabled2==undefined</el-button>
+    <el-button v-bind:disabled="Disabled3">Disabled3==false</el-button>
+    <el-button v-bind:disabled="Disabled">Disabled=true</el-button>
+    <h3>使用 JavaScript 表达式</h3>
+    <p>{{ number + 1 }}<el-tag effect="dark">{ {number + 1} }</el-tag></p>
+    <p>
+      {{ ok ? "YES" : "NO" }}
+      <el-tag effect="dark">{ {ok ? "YES" : "NO"} }</el-tag>
+    </p>
+    <p>
+      {{ message.split("").reverse().join("") }}
+      <el-tag effect="dark">{ {message.split("").reverse().join("")} }</el-tag>
+    </p>
+    <p v-bind:id="'list-' + id">
+      list-{{ id }}<el-tag effect="dark">{ {v-bind:id="'list-' + id"} }</el-tag>
+    </p>
+    <h2>指令</h2>
+    <h3>参数</h3>
+    <a v-bind:href="url">百度</a>
+    <el-tag effect="dark">v-bind:href="url"</el-tag>
+    <a v-on:click="doSomething">doSomething</a>
+    <el-tag effect="dark">v-on:click="doSomething"</el-tag>
   </div>
 </template>
 
@@ -11,17 +47,29 @@
 export default {
   data () {
     return {
-      msg: 'Message'
+      message: 'Message.',
+      rawHtml: '<span style="color:red">This should be red.</span>',
+      Disabled1: null,
+      Disabled2: undefined,
+      Disabled3: false,
+      Disabled: true,
+      number: 10000,
+      ok: true,
+      id: 201907249025,
+      url: 'http://www.baidu.com'
     }
   },
   mounted:function () { },
   methods: {
     handleChange() {
-      if(this.msg==='Message'){
-        this.msg='Message Change'
+      if(this.message==='Message.'){
+        this.message='Message Change.'
       } else{
-        this.msg = 'Message'
+        this.message = 'Message.'
       }
+    },
+    doSomething() {
+      alert('doSomething')
     }
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -6,9 +6,9 @@ actionLink: /guide/
 features:
   - title:
     details: 把所有的不快给昨天
-  - title: 
+  - title:
     details: 把所有的努力给今天
-  - title: 
+  - title:
     details: 把所有的希望给明天
 footer: MIT Licensed | Copyright © 2020-present qubeijun
 ---
